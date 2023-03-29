cdb2d4ca7113542bbca98f91c79a9eb906450893
eders <eders@PREMIEREDAMA>
2023-03-28 22:34:43 -0300

    Add project files.

---
+++ Fiap.Web.AspNet/Controllers/RepresentanteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.AspNet.Controllers
+{
+    public class RepresentanteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Fiap.Web.AspNet/Models/RepresentanteModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Fiap.Web.AspNet.Models
+{
+
+    public class RepresentanteModel
+    {
+
+        public int RepresentanteId { get; set; }
+
+        public string? Cpf { get; set; }
+
+        public string? NomeRepresentante { get; set; }
+
+        public string? Token { get; set; }
+
+        public RepresentanteModel()
+        {
+
+        }
+
+        public RepresentanteModel(int representanteId, string nomeRepresentante)
+        {
+            RepresentanteId = representanteId;
+            NomeRepresentante = nomeRepresentante;
+        }
+        public RepresentanteModel(int representanteId, string cpf, string nomeRepresentante)
+        {
+            RepresentanteId = representanteId;
+            Cpf = cpf;
+            NomeRepresentante = nomeRepresentante;
+        }
+    }
+}
